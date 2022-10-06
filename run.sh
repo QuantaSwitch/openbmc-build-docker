93c77b89e54476b40105a8fdfccb56f4d0a61a89
Audi Hsu <audi.hsu@quantatw.com>
2022-10-06 13:18:56 +0800

    Add build docker and clean up container in run.sh

--- run.sh
+++ run.sh
@@ -26,7 +26,25 @@ if [ ! -d "`realpath "$workdir"`" ]; then
     usage
 fi
 
+if [ -z "`docker images $docker_img --format '{{.Repository}}'`" ]; then
+    if [ -x ./build.sh ]; then
+        echo "Trying to build docker $docker_img"
+        ./build.sh
+    fi
+fi
+
+if [ -z "`docker images $user_docker_img --format '{{.Repository}}'`" ]; then
+    if [ -x ./build-user.sh ]; then
+        echo "Trying to build docker $user_docker_img"
+        ./build-user.sh
+    fi
+fi
+
 wd=$(realpath $workdir)
 
 docker run -it -v $wd:$wd -w $wd $user_docker_img /bin/bash
+
+# clean up
+echo "Clean up docker container from $user_docker_img"
+docker rm `docker ps -q --filter ancestor=$user_docker_img --filter status=exited`
 
